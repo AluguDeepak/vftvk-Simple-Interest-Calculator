e3c8720f868a40b274c51b2a939453315179897d
AluguDeepak <83202616+AluguDeepak@users.noreply.github.com>
2021-08-03 21:12:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
-    if(principal=="")
-    {
-        alert("Enter the amount");
-    }
     var rate=document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
@@ -15,4 +11,11 @@ function updateRate()
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
-              
+function validate()
+{
+    var v=document.getElementById("principal").value;
+    if(v=="")
+    {
+        alert("enter the value");
+    }
+}
